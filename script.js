366de487bd8d03c7d743b2390ab1c8a072ab5fb0
Anouar Jemai <anouar.salemjemai@gmail.com>
2024-01-31 21:53:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var a = {
+    firstname : "anouar",
+    lastname : "jemai",
+    age : 30,
+}
+
+for(var key in  a){
+    document.write( key + " : "+ a[key] + "<br>") ;
+
+}
+
+console.log(a);
+
+
+
+
+// document.write(hello);
+// console.log(hello);
+
+// for (var i = 0; i < std.length ; i++){
+
+//     document.write(std[i].age + "<br>");
+//     //document.write("<br>");
+// }
+
+
+
+
+
+
+// document.write(JSON.stringify(a));
+// document.write("<br>");
+
+//document.write((first.lastname));
+
+//console.log(first);
